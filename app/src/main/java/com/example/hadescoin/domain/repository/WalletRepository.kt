d51f718103650be7518ff89272f53e4f28576506
John Mesa <john.mesa@utp.edu.co>
2026-05-05 18:40:49 -0500

    eliminación de archivos no necesarios

--- app/src/main/java/com/example/hadescoin/domain/repository/WalletRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hadescoin.domain.repository
-
-import com.example.hadescoin.domain.model.WalletTransaction
-
-interface WalletRepository {
-	suspend fun getBalance(userId: String): Result<Double>
-	suspend fun getTransactions(userId: String): Result<List<WalletTransaction>>
-}
-
